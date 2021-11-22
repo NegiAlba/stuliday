7348772e0031d228f3a31388020d95c867ba7345
Axel Gromat <axel.gromat@gmail.com>
2021-11-22 10:44:54 +0100

    version procédurale + upload image fini

--- app/add-annonces_post.php
+++ app/add-annonces_post.php
@@ -18,6 +18,7 @@ if (empty($_POST['type']) || empty($_POST['capacity']) || empty($_POST['country'
     $price = htmlspecialchars(floatval($_POST['price']));
     $date_start = htmlspecialchars(trim($_POST['date_start']));
     $date_end = htmlspecialchars(trim($_POST['date_end']));
+    $id = $_SESSION['id'];
 
     if (!empty($_POST['location_adress'])) {
         $location_adress = htmlspecialchars(trim($_POST['location_adress']));
@@ -29,25 +30,21 @@ if (empty($_POST['type']) || empty($_POST['capacity']) || empty($_POST['country'
     } else {
         $description = null;
     }
-        
 
     if (empty($_FILES['image']['name'])) {
         $imagePath = 'uploads/wait.jpg';
         $image = null;
-        
-    }else {
+    } else {
         $image = $_FILES['image'];
     }
 }
 
-if (null !== $date_start && null !==$date_end && $date_end < $date_start) {
+if (null !== $date_start && null !== $date_end && $date_end < $date_start) {
     header('Location:add-annonces.php?error=pastAvailablity');
     exit();
 }
 
-
 if ($image) {
-    
     $valid_ext = ['jpg', 'jpeg', 'png'];
     $check_ext = strtolower(substr(strrchr($image['name'], '.'), 1));
 
@@ -68,7 +65,7 @@ if ($image) {
     }
 }
 
-$insertAnnonce = 'INSERT INTO location (type,capacity,location_adress,country,description,price,image,date_start,date_end) VALUES(:type,:capacity,:location_adress,:country,:description,:price,:image,:date_start,:date_end)';
+$insertAnnonce = 'INSERT INTO location (type,capacity,location_adress,country,description,price,image,date_start,date_end,id) VALUES(:type,:capacity,:location_adress,:country,:description,:price,:image,:date_start,:date_end,:id)';
 $reqInsertAnnonce = $connexion->prepare($insertAnnonce);
 $reqInsertAnnonce->bindValue(':type', $type, PDO::PARAM_STR);
 $reqInsertAnnonce->bindValue(':capacity', $capacity);
@@ -79,6 +76,7 @@ $reqInsertAnnonce->bindValue(':price', $price);
 $reqInsertAnnonce->bindValue(':image', $imagePath, PDO::PARAM_STR);
 $reqInsertAnnonce->bindValue(':date_start', $date_start, PDO::PARAM_STR);
 $reqInsertAnnonce->bindValue(':date_end', $date_end, PDO::PARAM_STR);
+$reqInsertAnnonce->bindValue(':id', $id);
 
 if ($reqInsertAnnonce->execute()) {
     header('Location:profil.php?success=addedProduct');
@@ -87,4 +85,3 @@ if ($reqInsertAnnonce->execute()) {
     header('Location:add-annonces.php?error=unknownError');
     exit();
 }
-?>
--- app/modifier-annonces.php
+++ app/modifier-annonces.php
@@ -12,21 +12,25 @@ include_once '_zoom-annonces.php';
 
 <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ps ps--active-y">
     <div class="card container m-4 p-4">
-       
+
         <form action="modifier-annonces_post.php" method="post" enctype="multipart/form-data">
-            <input type="text" class="form-control form-control-lg" name="type" value="<?php echo $annonce['type']; ?>"></input>
+            <input type="text" class="form-control form-control-lg" name="type"
+                value="<?php echo $annonce['type']; ?>"></input>
             <p class="text-bold">Capacité :</p>
             <input type="number" class="form-control" name="capacity" value="<?php echo $annonce['capacity']; ?>" />
             <hr>
             <p class="text-bold">Adresse de la location :</p>
-            <input type="text" class="form-control" name="location_adress" value="<?php echo $annonce['location_adress']; ?>" />
+            <input type="text" class="form-control" name="location_adress"
+                value="<?php echo $annonce['location_adress']; ?>" />
             <hr>
             <p class="text-bold">Pays :</p>
             <input type="text" class="form-control" name="country" value="<?php echo $annonce['country']; ?>" />
             <hr>
             <div class="col-md-4 center">
-            <p class="text-bold">Image de la location</p>
-            <input class="form-control" type="file" id="formFile" accept=".png,.jpg,.jpeg" name="image" value="<?php echo $annonce['image']; ?>">                
+                <p class="text-bold">Image de la location</p>
+                <input class="form-control" type="file" id="formFile" accept=".png,.jpg,.jpeg" name="image">
+                <input class="form-control" type="hidden" id="formFile" name="image_path"
+                    value="<?php echo $annonce['image']; ?>">
             </div>
             <p class="text-bold">Description :</p>
             <textarea name="description" class="form-control" rows=3><?php echo $annonce['description']; ?></textarea>
@@ -38,13 +42,14 @@ include_once '_zoom-annonces.php';
             <input type="date" class="form-control" name="date_end" value="<?php echo $annonce['date_end']; ?>" />
             <hr>
             <p class="text-bold">Prix :</p>
-            <input type="number" class="form-control col-2 mb-2" name="price" value="<?php echo $annonce['price']; ?>" />
-            
+            <input type="number" class="form-control col-2 mb-2" name="price"
+                value="<?php echo $annonce['price']; ?>" />
+
             <input type="hidden" name="id" value="<?php echo $annonce['location_id']; ?>">
             <button type="submit" class="btn btn-warning col-2">Modifier l'annonce</button>
         </form>
     </div>
-    
+
 </main>
 
 <?php
